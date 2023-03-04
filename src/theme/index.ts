196a1359c4018e476274281d1cecfda5bb87bcf0
Iann Rodrigues <iannrst11@gmail.com>
2023-03-04 11:12:06 -0300

    config: styled-components/global theme

---
+++ src/theme/index.ts
@@ -0,0 +1,35 @@
+export default {
+  COLORS: {
+    WHITE: '#FFFFFF',
+
+    GRAY_100: '#FAFAFA',
+    GRAY_200: '#DDDEDF',
+    GRAY_300: '#DDDEDF',
+    GRAY_400: '#B9BBBC',
+    GRAY_500: '#5C6265',
+    GRAY_600: '#333638',
+    GRAY_700: '#1B1D1E',
+
+    GREEN_100: '#E5F0D8',
+    GREEN_300: '#CBE4B4',
+    GREEN_700: '#639339',
+
+    RED_100: '#F4E6E7',
+    RED_300: '#F3BABD',
+    RED_700: '#BF3B44',
+  },
+
+  FONT_FAMILY: {
+    REGULAR: 'NunitoSans_400Regular',
+    BOLD: 'NunitoSans_700Bold'
+  },
+
+  FONT_SIZE: {
+    XS: 12,
+    SM: 14,
+    MD: 16,
+    LG: 18,
+    XL: 24,
+    XL2: 32
+  }
+}
