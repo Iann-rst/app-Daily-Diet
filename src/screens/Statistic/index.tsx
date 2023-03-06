209a1cb453235d0e2cb15d536bf0b659e91da66c
Iann Rodrigues <iannrst11@gmail.com>
2023-03-06 13:19:54 -0300

    feat: Screen Estatísticas

---
+++ src/screens/Statistic/index.tsx
@@ -0,0 +1,36 @@
+import { Header } from "@components/Header";
+import { StatsCard } from "@components/StatsCard";
+import { Content, InfoMeals, InfoMealsSubTitle, InfoMealsTitle, InfoView, Title } from "./styles";
+
+type Props = {
+  isDiet: boolean;
+  totalMeals: number;
+  totalMealsOnDiet: number;
+  totalOffDietMeals: number;
+  bestSequenceOfMealsWithinTheDiet: number;
+}
+
+
+export function Statistic() {
+  return (
+    <>
+      <Header isDiet={false} />
+      <Content style={{ marginTop: -32 }}>
+        <Title>Estatística gerais</Title>
+        <StatsCard title="22" subTitle="melhor sequência de pratos dentro da dieta" />
+        <StatsCard title="109" subTitle="refeições registradas" />
+        <InfoView>
+          <InfoMeals style={{ marginRight: 8 }} type="PRIMARY">
+            <InfoMealsTitle>99</InfoMealsTitle>
+            <InfoMealsSubTitle>refeições dentro da dieta</InfoMealsSubTitle>
+          </InfoMeals>
+
+          <InfoMeals type="SECONDARY">
+            <InfoMealsTitle>10</InfoMealsTitle>
+            <InfoMealsSubTitle>refeições fora da dieta</InfoMealsSubTitle>
+          </InfoMeals>
+        </InfoView>
+      </Content>
+    </>
+  )
+}
---
+++ src/screens/Statistic/styles.ts
@@ -0,0 +1,61 @@
+import { View } from 'react-native';
+import styled, { css } from 'styled-components/native';
+
+export type InfoDietStyleProps = 'PRIMARY' | 'SECONDARY';
+
+type Props = {
+  type: InfoDietStyleProps;
+}
+
+export const Content = styled.View`
+  flex: 1;
+  background-color: ${({ theme }) => theme.COLORS.WHITE};
+  padding: 24px;
+  border-top-left-radius: 20px;
+  border-top-right-radius: 20px;
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+margin-bottom: 23px;
+  ${({ theme }) => css`
+    color: ${theme.COLORS.GRAY_700};
+    font-size: ${theme.FONT_SIZE.XS}px;
+    font-family: ${theme.FONT_FAMILY.BOLD}
+  `}
+`;
+
+export const InfoView = styled.View`
+  width: 100%;
+  
+  max-height: 107px;
+  min-height: 107px;
+
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const InfoMeals = styled(View) <Props>`
+  flex: 1;
+  padding: 16px;
+  background-color: ${({ theme, type }) => type === 'PRIMARY' ? theme.COLORS.GREEN_100 : theme.COLORS.RED_100};
+  align-items: center;
+  justify-content: center;
+`;
+
+export const InfoMealsTitle = styled.Text`
+${({ theme }) => css`
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    color: ${theme.COLORS.GRAY_700};
+    font-size: ${theme.FONT_SIZE.XL}px;
+  `}
+`;
+
+export const InfoMealsSubTitle = styled.Text`
+${({ theme }) => css`
+  font-family: ${theme.FONT_FAMILY.REGULAR};
+  color: ${theme.COLORS.GRAY_600};
+  font-size: ${theme.FONT_SIZE.SM}px;
+  `}
+  text-align: center;
+`;
