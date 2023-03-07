ab09951c12a23c8393469be3ca6c5fc6258b8548
Iann Rodrigues <iannrst11@gmail.com>
2023-03-06 21:25:29 -0300

    feat: add Componente Filter

--- src/screens/NewMeal/index.tsx
+++ src/screens/NewMeal/index.tsx
@@ -1,35 +1,56 @@
 import { Button } from "@components/Button";
+import { Filter } from "@components/Filter";
 import { Input } from "@components/Input";
 import { SecondaryHeader } from "@components/SecondaryHeader";
 import { useNavigation } from "@react-navigation/native";
-import { View } from "react-native";
+import { useState } from "react";
+import { Keyboard, View } from "react-native";
 
-import { Form, Content } from "./styles";
+import { Form, Content, LabelDiet, Container, Groups } from "./styles";
 
 export function NewMeal() {
   const { navigate } = useNavigation();
+
+  const [diet, setDiet] = useState<boolean | ''>('');
 
   function handleGoBack() {
     navigate("home")
   }
 
   return (
-    <>
+    <Container>
       <SecondaryHeader title="Nova refeição" goBack={handleGoBack} />
-      <Content>
+      <Content onPress={Keyboard.dismiss}>
         <Form>
           <Input label="Nome" />
           <Input label="Descrição" type="PRIMARY" textAlignVertical="top" />
-          <View style={{
-            flexDirection: 'row', justifyContent: 'space-between'
-          }}
-          >
+
+          <Groups style={{ gap: 20 }}>
             <Input label="Data" type="SECONDARY" />
             <Input label="Hora" type="SECONDARY" />
-          </View>
+          </Groups>
+
+          <LabelDiet>Está dentro da dieta?</LabelDiet>
+
+          <Groups style={{ gap: 8 }}>
+            <Filter
+              title="Sim"
+              isActive={diet === true}
+              onPress={() => setDiet(true)}
+            />
+
+            <Filter
+              title="Não"
+              diet="NO"
+              isActive={diet === false}
+              onPress={() => setDiet(false)}
+            />
+          </Groups>
+
+          <Button style={{ marginTop: 172 }} title="Cadastrar refeição" />
         </Form>
-        <Button title="Cadastrar refeição" />
+
       </Content>
-    </>
+    </Container>
   )
 }
