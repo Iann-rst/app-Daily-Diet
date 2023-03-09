7f2b8f19a7ed3b17a469138fc0cca9063f14c257
Iann Rodrigues <iannrst11@gmail.com>
2023-03-09 16:29:13 -0300

    feat: Criação de uma nova refeição

---
+++ src/storage/meals/createNewMeals.ts
@@ -0,0 +1,33 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Meal, Meals, MEALS_COLLECTION } from "@storage/storageConfig";
+import { getAllMeals } from "./getAllMeals";
+
+export async function mealsCreate(newMeal: Meal) {
+  try {
+
+    const storedMeals = await getAllMeals();
+
+    const mealDateExists = storedMeals.findIndex(meal => meal.title === newMeal.date);
+
+    if (mealDateExists > -1) {
+      //Já existe uma posição com essa data da refeição
+
+      storedMeals[mealDateExists].data = [...storedMeals[mealDateExists].data, newMeal];
+      await AsyncStorage.setItem(MEALS_COLLECTION, JSON.stringify(storedMeals));
+
+    } else {
+      // Não existe uma posição com essa data
+
+      const newMealData: Meals = {
+        title: newMeal.date,
+        data: [newMeal]
+      }
+
+      const storage = JSON.stringify([...storedMeals, newMealData]);
+      await AsyncStorage.setItem(MEALS_COLLECTION, storage);
+    }
+
+  } catch (error) {
+    throw error;
+  }
+}
