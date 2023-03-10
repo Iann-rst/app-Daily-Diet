2d99be0ab76572b12f770a6c899c36d6c9fbd1b9
Iann Rodrigues <iannrst11@gmail.com>
2023-03-10 16:57:49 -0300

    feat: Pegar todas as estatística gerais

--- src/screens/Statistic/index.tsx
+++ src/screens/Statistic/index.tsx
@@ -1,6 +1,9 @@
 import { Header } from "@components/Header";
 import { StatsCard } from "@components/StatsCard";
 import { useNavigation } from "@react-navigation/native";
+import { getAllMealStats } from "@utils/getAllMealStats";
+import { useEffect, useState } from "react";
+
 import {
   Container,
   Content,
@@ -12,37 +15,48 @@ import {
 } from "./styles";
 
 type Props = {
-  isDiet: boolean;
   totalMeals: number;
-  totalMealsOnDiet: number;
-  totalOffDietMeals: number;
-  bestSequenceOfMealsWithinTheDiet: number;
+  numberOfMealsInDiet: number;
+  numberOfOffDietMeals: number;
+  isOnDiet: boolean
+  bestDietSequence: number;
+  percentageDietMeal: string;
 }
 
 
 export function Statistic() {
 
   const { navigate } = useNavigation();
+  const [statistic, setStatistic] = useState<Props>({} as Props);
 
   function handleGoBack() {
     navigate("home");
   }
 
+  async function handleGetStatistic() {
+    const status = await getAllMealStats();
+    setStatistic(status);
+  }
+
+  useEffect(() => {
+    handleGetStatistic();
+  }, [])
+
   return (
     <Container>
-      <Header goBack={handleGoBack} isDiet={true} />
+      <Header goBack={handleGoBack} isDiet={statistic.isOnDiet} percentageDietMeal={statistic.percentageDietMeal} />
       <Content style={{ marginTop: -32 }}>
         <Title>Estatística gerais</Title>
-        <StatsCard title="22" subTitle="melhor sequência de pratos dentro da dieta" />
-        <StatsCard title="109" subTitle="refeições registradas" />
+        <StatsCard title={String(statistic.bestDietSequence)} subTitle="melhor sequência de pratos dentro da dieta" />
+        <StatsCard title={String(statistic.totalMeals)} subTitle="refeições registradas" />
         <InfoView>
           <InfoMeals style={{ marginRight: 8 }} type="PRIMARY">
-            <InfoMealsTitle>99</InfoMealsTitle>
+            <InfoMealsTitle>{statistic.numberOfMealsInDiet}</InfoMealsTitle>
             <InfoMealsSubTitle>refeições dentro da dieta</InfoMealsSubTitle>
           </InfoMeals>
 
           <InfoMeals type="SECONDARY">
-            <InfoMealsTitle>10</InfoMealsTitle>
+            <InfoMealsTitle>{statistic.numberOfOffDietMeals}</InfoMealsTitle>
             <InfoMealsSubTitle>refeições fora da dieta</InfoMealsSubTitle>
           </InfoMeals>
         </InfoView>
