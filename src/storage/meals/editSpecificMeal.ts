016e0bb393d6742c5bd8975f123493ed8d4d5ff6
Iann Rodrigues <iannrst11@gmail.com>
2023-03-10 17:53:24 -0300

    feat: Editar uma refeição especifica

--- src/storage/meals/editSpecificMeal.ts
+++ src/storage/meals/editSpecificMeal.ts
@@ -1,9 +1,18 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { getAllMeals } from '@storage/meals/getAllMeals';
-import { Meal } from '@storage/storageConfig';
+import { Meal, MEALS_COLLECTION } from '@storage/storageConfig';
 
-export async function EditSpecificMeal(meal: Meal) {
+export async function editSpecificMeal(updateMeal: Meal) {
+  const meals = await getAllMeals();
+  const indexMealGroup = meals.findIndex(group => group.title === updateMeal.date);
+  if (indexMealGroup > -1) {
+    const meal = meals[indexMealGroup];
 
-  /** Editar uma refeição */
-  const storageMeals = await getAllMeals();
+    const indexMeal = meal.data.findIndex(meal => meal.id === updateMeal.id);
 
+    if (indexMeal > -1) {
+      meal.data[indexMeal] = updateMeal;
+      await AsyncStorage.setItem(MEALS_COLLECTION, JSON.stringify(meals));
+    }
+  }
 }
