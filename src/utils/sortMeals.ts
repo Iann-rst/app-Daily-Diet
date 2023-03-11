3e937804cbcfeaf9626ab294d9b5f1f400b78ad0
Iann Rodrigues <iannrst11@gmail.com>
2023-03-10 22:01:42 -0300

    feat: Ordenar as refeições pela data

---
+++ src/utils/sortMeals.ts
@@ -0,0 +1,9 @@
+import { Meal } from '@storage/storageConfig';
+/** Função para ordenar as refeições de acordo com a data e com a hora de cada uma
+ * 
+ * https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+ */
+
+export async function sortMeals(meals: Meal[]) {
+
+}
