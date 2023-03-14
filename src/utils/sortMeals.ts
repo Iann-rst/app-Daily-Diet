177b206002529f15df726b170aeabbacb26d7638
Iann Rodrigues <iannrst11@gmail.com>
2023-03-14 10:49:01 -0300

    feat: Ordenação das refeições pela data e hora

--- src/utils/sortMeals.ts
+++ src/utils/sortMeals.ts
@@ -1,9 +1,28 @@
-import { Meal } from '@storage/storageConfig';
-/** Função para ordenar as refeições de acordo com a data e com a hora de cada uma
- * 
- * https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
- */
+import { Meal, Meals } from '@storage/storageConfig';
+/** Função para ordenar as refeições de acordo com a data e com a hora de cada uma **/
 
-export async function sortMeals(meals: Meal[]) {
+export async function sortMeals(meals: Meals[]) {
+  meals.sort((a: Meals, b: Meals) => {
+    if (a.title > b.title) {
+      return 1
+    }
+    if (a.title < b.title) {
+      return -1;
+    }
+    return 0;
+  })
 
+  meals.map(meal => {
+    meal.data.sort((a: Meal, b: Meal) => {
+      if (a.hour > b.hour) {
+        return 1
+      }
+      if (a.hour < b.hour) {
+        return -1;
+      }
+      return 0;
+    })
+  })
+
+  return meals;
 }
